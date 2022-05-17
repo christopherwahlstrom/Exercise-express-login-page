48c6e2d61956c5062ec0d84756eabfd9640a32b2
Christopher Wahlström <83230387+christopherwahlstrom@users.noreply.github.com>
2022-05-17 19:24:55 +0200

    Finished
    Exercise node.js with encryption bcrypt

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
-const { hash } = require('bcrypt');
 
+
+app.use(express.json())
 
 const users = [
 ]
@@ -14,10 +15,8 @@ app.get('/users', (req, res) => {
 
 app.post('/users', async (req, res) => {
     try {
-        const salt =await bcrypt.genSalt()
-        const hashedPassword = await bcrypt.hash(req.body.password, salt)
-        console.log(salt)
-        console.log(hashedPassword)
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
         const user = {
             name: req.body.name,
             password: hashedPassword
@@ -26,8 +25,23 @@ app.post('/users', async (req, res) => {
         res.status(201).send()        
     } catch {
         res.status(500).send()
+    }  
+})
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name = req.body.name)
+    if (user == null) {
+        return res.status(400).send('User not found')
     }
-    
+    try {
+       if (await bcrypt.compare(req.body.password, user.password)) {
+           res.send('Success')
+       } else {
+           res.send('Not allowed')
+        }
+    } catch {
+        res.status(500).send()
+    }
 })
 
 
